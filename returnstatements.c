c6b0dfe1cdccac3fca64f7d6b6a28d40de485fe8
Jon Cruxible <tagrous87@gmail.com>
2022-10-15 14:58:09 -0400

    Add files via upload

---
+++ returnstatements.c
@@ -0,0 +1,18 @@
+//Return Statements
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double cube (double num);
+
+int main(){
+
+	printf("Answer: %f", cube(7.0));
+
+	return 0;
+}
+
+double cube(double num){
+	double result = num * num * num;
+	return result;
+}
